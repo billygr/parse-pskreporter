fde977a80111e23a5a2b34efecfbdef4db5c616c
billygr <billygr@users.noreply.github.com>
2016-05-26 21:32:18 +0300

    More frequencies, added another output

--- parse-psk-freq.py
+++ parse-psk-freq.py
@@ -10,12 +10,14 @@ import re
 print "Content-type: text/plain; charset=us-ascii\r\n"
 
 #Pending 160m frequencies 1820000
+
 rep = {
        "3500000" : "80m", "3510000" : "80m", "3530000" : "80m",
        "3580000" : "80m", "3680000" : "80m",
 
        "7000000" : "40m", "7010000" : "40m", "7020000" : "40m",
-       "7030000" : "40m", "7040000" : "40m", "7080000" : "40m",
+       "7030000" : "40m", "7040000" : "40m", "7050000" : "40m",
+       "7080000" : "40m",
 
        "10100000" : "30m", "10110000" : "30m", "10120000" : "30m",
        "10130000" : "30m", "10140000" : "30m",
@@ -24,21 +26,23 @@ rep = {
        "14030000" : "20m", "14040000" : "20m", "14050000" : "20m",
        "14060000" : "20m", "14070000" : "20m", "14080000" : "20m",
        "14090000" : "20m", "14100000" : "20m", "14110000" : "20m",
-       "14120000" : "20m",
+       "14120000" : "20m", "14240000" : "20m", "14260000" : "20m",
 
        "18000000" : "17m", "18070000" : "17m", "18080000" : "17m",
        "18090000" : "17m", "18100000" : "17m", "18110000" : "17m",
        "18150000" : "17m",
 
        "21000000" : "15m", "21010000" : "15m", "21020000" : "15m",
-       "21030000" : "15m", "21040000" : "15m", "21070000" : "15m",
-       "21080000" : "15m", "21090000" : "15m",
+       "21030000" : "15m", "21040000" : "15m", "21060000" : "15m",
+       "21070000" : "15m", "21080000" : "15m", "21090000" : "15m",
+       "21270000" : "15m",
 
        "24890000" : "12m", "24900000" : "12m", "24910000" : "12m",
 
-       "28010000" : "10m", "28020000" : "10m", "28030000" : "10m",
-       "28040000" : "10m", "28070000" : "10m", "28080000" : "10m",
-       "28120000" : "10m", "28140000" : "10m",
+       "28000000" : "10m", "28010000" : "10m", "28020000" : "10m",
+       "28030000" : "10m", "28040000" : "10m", "28070000" : "10m",
+       "28080000" : "10m", "28120000" : "10m", "28140000" : "10m",
+       "28450000" : "10m",
 
        "50100000" : "6m", "50280000" : "6m"
 } 
@@ -70,6 +74,9 @@ psk_wavelength_list.sort()
 # frequency score #spots #tx #rx
 # grid KM%, 15 mins
 
+# Use a list for keeping the bands
+band_list = []
+
 score6m = 0
 score10m = 0
 score12m = 0
@@ -95,35 +102,45 @@ for wavelength in psk_wavelength_list:
   if m[0] == "6m":
     score6m = score6m + int(m[1])
     spots6m = spots6m + int(m[2])
+    band_list.append("6m")
   elif m[0] == "10m":
     score10m = score10m + int(m[1])
     spots10m = spots10m + int(m[2])
+    band_list.append("10m")
   elif m[0] == "12m":
     score12m = score12m + int(m[1])
     spots12m = spots12m + int(m[2])
+    band_list.append("12m")
   elif m[0] == "15m":
     score15m = score15m + int(m[1])
     spots15m = spots15m + int(m[2])
+    band_list.append("15m")
   elif m[0] == "17m":
     score17m = score17m + int(m[1])
     spots17m = spots17m + int(m[2])
+    band_list.append("17m")
   elif m[0] == "20m":
     score20m = score20m + int(m[1])
     spots20m = spots20m + int(m[2])
+    band_list.append("20m")
   elif m[0] == "30m":
     score30m = score30m + int(m[1])
     spots30m = spots30m + int(m[2])
+    band_list.append("30m")
   elif m[0] == "40m":
     score40m = score40m + int(m[1])
     spots40m = spots40m + int(m[2])
+    band_list.append("40m")
   elif m[0] == "80m":
     score80m = score80m + int(m[1])
     spots80m = spots80m + int(m[2])
+    band_list.append("80m")
 # just in case more frequencies added 
   else:
     print wavelength
 
-print "6m score:", score6m, "spots:", spots6m
+# Score displayed is the SUM of all scores provided for all frequencies in this band
+print " 6m score:", score6m, "spots:", spots6m
 print "10m score:", score10m, "spots:", spots10m
 print "12m score:", score12m, "spots:", spots12m
 print "15m score:", score15m, "spots:", spots15m
@@ -131,4 +148,14 @@ print "17m score:", score17m, "spots:", spots17m
 print "20m score:", score20m, "spots:", spots20m
 print "30m score:", score30m, "spots:", spots30m
 print "40m score:", score40m, "spots:", spots40m
-print "80m score:", score80m, "spots:", spots80m
+print "80m score:", score80m, "spots:", spots80m,"\r\n"
+
+print " 6m Frequencies:",  band_list.count("6m"), "spots:",spots6m
+print "10m Frequencies:", band_list.count("10m"), "spots:",spots10m
+print "12m Frequencies:", band_list.count("12m"), "spots:",spots12m
+print "15m Frequencies:", band_list.count("15m"), "spots:",spots15m
+print "17m Frequencies:", band_list.count("17m"), "spots:",spots17m
+print "20m Frequencies:", band_list.count("20m"), "spots:",spots20m
+print "30m Frequencies:", band_list.count("30m"), "spots:",spots30m
+print "40m Frequencies:", band_list.count("40m"), "spots:",spots40m
+print "80m Frequencies:", band_list.count("80m"), "spots:",spots80m
